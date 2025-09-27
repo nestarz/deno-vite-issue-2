7e31620f8078ab0e8715c927394b20d3feb4ad24
Elias Rhouzlane <elias.rhouzlane@gmail.com>
2025-09-27 23:45:15 +0200

    fix: name

--- cjs-package/index.js
+++ cjs-package/index.js
@@ -1,5 +1,4 @@
 const id = Math.random();
-// This log will appear in your console each time the module is executed.
-console.log(`[my-cjs-module] Module instance created with ID: ${id}`);
+console.log(`[noop-tag] Module instance created with ID: ${id}`);
 
 module.exports = { id };
