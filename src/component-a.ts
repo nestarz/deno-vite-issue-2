29280ff820ad8a6ff8e10206923b864621bd45b9
Elias Rhouzlane <elias.rhouzlane@gmail.com>
2025-09-27 23:42:33 +0200

    fix: unused comments

--- src/component-a.ts
+++ src/component-a.ts
@@ -1,5 +1,3 @@
-// Imports using the bare specifier.
-// Vite's dependency scanner is likely to recognize this.
 import state from "noop-tag";
 
 export const idFromA = state.id;
--- src/component-b.ts
+++ src/component-b.ts
@@ -1,5 +1,3 @@
-// Imports using the `npm:` specifier.
-// This is handled by your custom deno() plugin.
 import state from "npm:noop-tag";
 
 export const idFromB = state.id;
